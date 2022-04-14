6f86f091471a890cfed15f5a2cc20865f8311570
Otavio Eduardo Antonio Barbosa <otavio.barbosa@castgroup.com.br>
2022-04-14 09:57:27 -0300

    teste

---
+++ src/test/java/homepage/HomePageTests.java
@@ -0,0 +1,73 @@
+package homepage;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.Test;
+
+import base.BaseTests;
+import pages.LoginPage;
+import pages.ProdutoPage;
+
+public class HomePageTests extends BaseTests {
+
+	 @Test
+	 public void testcontarProdutos_oitoProdutosDiferentes() {
+		 carregarPaginaInicial();
+		 assertThat(homePage.contarProdutos(), is(8));
+	 }
+	 
+	 @Test
+	 public void testValidarCarrinhoZerado_ZeroItensNoCarrinho() {
+		 int produtosNoCarrinho = homePage.obterQuantidadeProdutosNoCarrinho();		 
+		 assertThat(produtosNoCarrinho, is(0));
+	 }
+	 
+	 @Test
+	 public void testValidarDetalhesDoProduto_DescricaoEValorIguais() {
+		 int indice = 0;
+		 String nomeProduto_HomePage = homePage.obterNomeProduto(indice);
+		 String precoProduto_HomePage = homePage.obterPrecoProduto(indice);
+		 
+		 System.out.println(nomeProduto_HomePage);
+		 System.out.println(precoProduto_HomePage);
+		 
+		 ProdutoPage produtoPage = homePage.clicarProduto(indice);
+		 
+		 String nomeProduto_ProdutoPage = produtoPage.obterNomeProduto();
+		 String precoProduto_ProdutoPage = produtoPage.obterPrecoProduto();
+		  
+		 System.out.println(nomeProduto_ProdutoPage);
+		 System.out.println(precoProduto_ProdutoPage);
+		 
+		 assertThat(nomeProduto_HomePage.toUpperCase(), is(nomeProduto_ProdutoPage.toUpperCase()));
+		 //assertThat(precoProduto_HomePage, is(precoProduto_ProdutoPage));
+	 }
+	 
+	 @Test
+	 public void testLoginComSucesso_UsuarioLogado() {
+		 //Clicar no botão Sign In na gome page
+		 LoginPage loginPage = homePage.clicarBotaoSingIn();
+		 
+		 //Preencher usuario e senha
+		  loginPage.preencherEmail("otavioedu1@hotmail.com");
+		  loginPage.preencherPassword("otavio123");
+		 
+		 //Clicar na botão Sing In para logar
+		 loginPage.clicarBotaoSingIn();
+		 
+		 //Validar se o usuário está logado de fato
+		 assertThat(homePage.estaLogado("Otávio Barbosa"), is(true));
+		 
+		 carregarPaginaInicial();
+	 }
+	 
+	 @Test
+	 public void incluirProdutoNoCarrinho_ProdutoIncluidoComSucesso() {
+		 if(!homePage.estaLogado("Otávio Barbosa")) {
+			 testLoginComSucesso_UsuarioLogado();
+		 }
+		 testValidarDetalhesDoProduto_DescricaoEValorIguais(); 
+		 
+	 }
+}
